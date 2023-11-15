8b2379dc9ca0c71a5cddf72ab0b36485a92d9ed8
noah-whelann <noah.whelan@berkeley.edu>
2023-11-14 20:05:19 -0800

    Added models for both the User and a Game

--- my-app/app/api/route.ts
+++ my-app/app/api/route.ts
@@ -1,4 +1,9 @@
 import { NextRequest } from "next/server";
+import { z } from "zod";
+
+z.object({ //give the shape of the object in the body of the request
+
+})
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
