cfc870d8382e8aee91a26c2ff28dee5398aaa633
noah-whelann <noah.whelan@berkeley.edu>
2023-11-16 13:13:05 -0800

    Implementing sign in form

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1></h1>
+    <main className="flex min-h-screen  items-center justify-between">
+      <h1>Welcome to typed!</h1>
+      <Link href="/login">Sign In</Link>
+      <Link href="/register">Get Started</Link>
     </main>
   );
 }
