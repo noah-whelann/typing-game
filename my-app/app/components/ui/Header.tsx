805905fa6c424902ec8e881d6cbb4a5facdf1245
noah-whelann <noah.whelan@berkeley.edu>
2023-11-17 17:28:39 -0800

    Added users name

--- my-app/app/components/ui/Header.tsx
+++ my-app/app/components/ui/Header.tsx
@@ -3,15 +3,21 @@ import Image from "next/image";
 import logo from "@/app/assets/logo.png"
 import { CgProfile } from "react-icons/cg";
 import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-const Header = () => {
+const Header = async () => {
+
+  const session = await getServerSession(authOptions);
+
   return (
     <div className="flex justify-between items-center max-w-screen">
       <div className="w-20 m-10">
         <Image src={logo} alt="logo" />
       </div>
       <div className="flex items-center mx-20">
-        <p className="mx-3">name</p>
+        
+        <p className="mx-3">{session?.user?.name}</p>
 
         <div className="dropdown">
           <CgProfile tabIndex={0} className="cursor-pointer" />
