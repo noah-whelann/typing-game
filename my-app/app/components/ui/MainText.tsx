e32613b37a318d2156fc266e641ad1e7d102b1ea
Koa Lee <koalee@berkeley.edu>
2023-11-21 15:27:35 -0800

    Renders text as a list of chars

--- my-app/app/components/ui/MainText.tsx
+++ my-app/app/components/ui/MainText.tsx
@@ -5,7 +5,9 @@ import { useState } from "react";
 
 const MainText = () => {
     /* Logic Implementation required here */
-    let [text, setText] = useState('replace this with api call for random word')
+    let [wordsList, setWordsList] = useState('replace this with api call for random word')
+    let [word, setWord] = useState('Random word')
+    let [text, setText] = useState(word)
 
     function textFocus() {
         /* Used to detect focus and stuff, can be changed */
@@ -14,17 +16,30 @@ const MainText = () => {
     }
 
     function updateText() {
+        /* Gets text input from hidden textinput */
         let input = document.getElementById("wordinput") as HTMLInputElement;
         if (input.value == "") {
-            setText('replace this with api call for random word');
+            setText(word);
         } else {
+            setText("");
             setText(input.value);
         }
+
+        renderText();
+    }
+
+    function renderText() {
+        let chars = Array.from(text);
+        return (
+            chars.map((c:string, i:number)=><span key={i}>{c}</span>)
+        )
     }
 
     return (
         <div className="textdisplay">
-            <p id="maintext" onClick={ textFocus }>{ text }</p>
+            <div id="maintext" onClick={ textFocus }>
+                {renderText()}
+            </div>
             <input id="wordinput" autoFocus 
             autoComplete="false" 
             onInput={ updateText } 
