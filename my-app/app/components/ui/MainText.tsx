971512d0d4767a88f39c97ae53ce430e08998cee
Koa Lee <koalee@berkeley.edu>
2023-11-20 16:59:07 -0800

    Added input for typing logic

--- my-app/app/components/ui/MainText.tsx
+++ my-app/app/components/ui/MainText.tsx
@@ -6,9 +6,22 @@ import { useState } from "react";
 const MainText = () => {
     /* Logic Implementation required here */
     let [text, setText] = useState('replace this with api call for random word')
+
+    function textFocus() {
+        /* Used to detect focus and stuff, can be changed */
+        let input = document.getElementById("wordinput");
+        if (input != null) input.focus();
+    }
+
+    function updateText() {
+        let input = document.getElementById("wordinput") as HTMLInputElement;
+        setText(input.value);
+    }
+
     return (
         <div className="textdisplay">
-            <p id="maintext">{ text }</p>
+            <p id="maintext" onClick={ textFocus }>{ text }</p>
+            <input id="wordinput" autoFocus autoComplete="false" onInput={ updateText }/>
             <button>
                 <VolumeUpRounded id="sound"/>
             </button>
