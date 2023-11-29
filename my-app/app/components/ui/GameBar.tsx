2e4d3167cc20c9843c8adf82b3402fa8ce78ace3
David Yu <davidyu295295@yahoo.com>
2023-11-28 16:25:24 -0800

    implemented sliding underlines for times and difficulties

--- my-app/app/components/ui/GameBar.tsx
+++ my-app/app/components/ui/GameBar.tsx
@@ -1,23 +1,49 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
 import "./GameBar.css"
 import { Settings} from '@mui/icons-material';
 import { NavigateNextRounded } from '@mui/icons-material';
 import { NavigateBeforeRounded } from '@mui/icons-material';
 
 const GameBar = () => {
+
+  const [activeTimeClass, setActiveTimeClass] = useState('hrtime ml-[7.5%]');
+  const [activeDiffClass, setActiveDiffClass] = useState('hrdiff ml-[7.5%] w-[16%]');
+  const handleTimeClick = (id: React.SetStateAction<string>) => {
+    if (id == 'timeone') {setActiveTimeClass('hrtime ml-[7.5%]');}
+    if (id == 'timetwo') {setActiveTimeClass('hrtime ml-[32.5%]');}
+    if (id == 'timethree') {setActiveTimeClass('hrtime ml-[57.5%]');}
+    if (id == 'timefour') {setActiveTimeClass('hrtime ml-[82.5%]');}
+  };
+  
+
+  const handleDiffClick = (id: React.SetStateAction<string>) => {
+    if (id == 'easy') {setActiveDiffClass('hrdiff ml-[7.5%] w-[16%]');}
+    if (id == 'medium') {setActiveDiffClass('hrdiff ml-[38%] w-[25%]');}
+    if (id == 'hard') {setActiveDiffClass('hrdiff ml-[77%] w-[16%]');}
+  }
+
   return (
     <div className="gamebar">
       <div id="navbar">
-        <button id="timeselection">10s</button>
-        <button id="timeselection">15s</button>
-        <button id="timeselection">30s</button>
-        <button id="timeselection">60s</button>
+        <div className="time">
+          <button className="timeselection" onClick={() => handleTimeClick('timeone')} id="timeone">10s</button>
+          <button className="timeselection" onClick={() => handleTimeClick('timetwo')} id="timetwo">15s</button>
+          <button className="timeselection" onClick={() => handleTimeClick('timethree')} id="timethree">30s</button>
+          <button className="timeselection" onClick={() => handleTimeClick('timefour')} id="timefour">60s</button>
+          <hr className={ activeTimeClass }/>
+        </div>
         <p id="separator">|</p>
-        <button id="difficulty">easy</button>
-        <button id="difficulty">medium</button>
-        <button id="difficulty">hard</button>
+        <div className="diff-container">
+          <button className="difficulty" onClick={() => handleDiffClick('easy')} id="easy">easy</button>
+          <button className="difficulty" onClick={() => handleDiffClick('medium')} id="medium">medium</button>
+          <button className="difficulty" onClick={() => handleDiffClick('hard')} id="hard">hard</button>
+          <hr className={ activeDiffClass }/>
+        </div>
         <p id="separator">|</p>
-        <button className='colortheme' id="darkmode">dark</button>
+        <button className='colortheme' id="darkmode">
+          <p className='hover-underline-animation'>dark</p>
+          </button>
         <button id="gamebar-icons">
         <Settings id='gamebaricons'/>
         </button>
@@ -27,7 +53,6 @@ const GameBar = () => {
         <button id="gamebar-icons">
         <NavigateNextRounded id='gamebaricons'/>
         </button>
-        
       </div>
     </div>
   );
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
           <p id="slogan-text">Learn to <u>type</u> <em>faster</em>, and <em>just better</em>.</p>
       </div>
         <div className="right-screen">
-            <Link href="/auth/signin" className='links' id='front-sign-in'>
-              <p className="hover-underline-animation">Sign In</p>
-              </Link>
             <Link href="/auth/signup" className='links' id='front-sign-up'>
               <p className="hover-underline-animation">Get Started</p>
+              </Link>
+            <Link href="/auth/signin" className='links' id='front-sign-in'>
+              <p className="hover-underline-animation">Sign In</p>
               </Link>
             <Link href="/dashboard" className='links' id='play-as-guest'>
               <p className="hover-underline-animation">Play as Guest</p>
